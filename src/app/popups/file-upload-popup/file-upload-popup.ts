dea087e788c5f187b293a8ddbf3b688515d7ef1d
Alex Hovansky <alex.hovansky@gmail.com>
2017-08-01 23:03:21 +0300

    UploadService fixes

--- src/app/popups/file-upload-popup/file-upload-popup.ts
+++ src/app/popups/file-upload-popup/file-upload-popup.ts
@@ -1,10 +1,12 @@
 import {Component} from "@angular/core";
 import {NgForm} from '@angular/forms';
+import {NgStyle} from '@angular/common';
 import {PopupService} from "../../services/popup.service";
 import {PopupContent} from "../../modules/popup/popup.content.module";
 import {LoadState} from "../../state/load.state";
 import {UploadService} from "../../services/upload.service";
 import {UploadItem} from "../../interface";
+import {Observable} from "rxjs";
 
 @Component({
     selector: 'file-upload-popup',
@@ -21,11 +23,40 @@ export class FileUploadPopup extends PopupContent {
         this.item = new UploadItem();
     }
 
+    getUploadedPercent() {
+        return this.uploadService.getProgress();
+    }
+
+    getUploadedPart() {
+        return Math.round(this.item.duration * this.uploadService.getProgress() / 100) || 0;
+    }
+
     prepareToUploadFile($event) {
         this.item.file = $event.srcElement.files[0];
+
+        this.checkFileDuration();
+    }
+
+    private checkFileDuration() {
+        let audio = new Audio();
+        audio.src = URL.createObjectURL(this.item.file);
+
+        audio.addEventListener("loadedmetadata", () => {
+            this.item.duration = Math.round(audio.duration);
+        });
+    }
+
+    private isUploadable() {
+        return true;
     }
 
     uploadFile(f: NgForm) {
-        this.uploadService.upload(this.item);
+        if (this.isUploadable()) {
+            this.uploadService.upload(this.item);
+
+            this.uploadService.onProgressUpload = (item, percentComplete) => {
+                console.log(item, percentComplete);
+            };
+        }
     }
 }
